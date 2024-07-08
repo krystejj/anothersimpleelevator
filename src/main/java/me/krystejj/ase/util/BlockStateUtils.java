c62b2a1c0caa557c45d9d9e794697c8452d82931
Krystian Wiecheć <krystian.wiechec.pub@krystejj.me>
2024-07-09 00:02:03 +0200

    Update to 1.0.2 - now it is possible to teleport when torch, sign etc. is on the wall above elevator

--- src/main/java/me/krystejj/ase/util/BlockStateUtils.java
+++ src/main/java/me/krystejj/ase/util/BlockStateUtils.java
@@ -6,13 +6,17 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
-public class TagUtils {
+public class BlockStateUtils {
     public static boolean isBlockElevator(BlockState blockState) {
         return blockHasTag(blockState, Identifier.of(ASE.MOD_ID, "elevators"));
     }
 
     public static boolean isBlockCarpet(BlockState blockState) {
         return blockHasTag(blockState, Identifier.of(ASE.MOD_ID, "carpets"));
+    }
+
+    public static boolean isAirOrWallBlock(BlockState blockState) {
+        return blockState.isAir() || blockHasTag(blockState, Identifier.of(ASE.MOD_ID, "wall_blocks"));
     }
 
     public static boolean blockHasTag(BlockState blockState, Identifier tagId) {
